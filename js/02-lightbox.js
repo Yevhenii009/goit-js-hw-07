7c4796a110d87bc76b7cdc939268459af9872e25
Yevhenii009 <stifgun@gmail.com>
2022-12-13 19:32:21 +0200

    дз2-7

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,58 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const galeryContainer = document.querySelector('.gallery');
+
+const createGalleryItemsMarkup =
+    galleryItems.map(({ preview, original, description }) => 
+    `
+    <a class="gallery__item" href="${original}">
+    <img class="gallery__image"
+    src="${preview}"
+    alt="${description}"/>
+    </a>
+    `
+    ).join('');
+
+galeryContainer.insertAdjacentHTML('beforeend', createGalleryItemsMarkup);
+    
+const lightbox = new SimpleLightbox('.gallery a', { 
+    captionsData: 'alt',
+    captionDelay: 250,
+});
+       
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 console.log(galleryItems);
