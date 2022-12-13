58ea54749a099b74817974cc7738f763c2510957
Yevhenii009 <stifgun@gmail.com>
2022-12-13 17:46:13 +0200

    дз1-7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,49 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galeryContainer = document.querySelector('.gallery');
+galeryContainer.addEventListener('click', onGaleryClick);
+
+const createGalleryItemsMarkup =
+    galleryItems.map(({ preview, original, description }) => 
+    `
+    <div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img class="gallery__image"
+    src="${preview}"
+    data-source="${original}"
+    alt="${description}"/>
+    </a></div>
+    `
+    ).join('');
+
+galeryContainer.insertAdjacentHTML('beforeend', createGalleryItemsMarkup);
+    
+function onGaleryClick(event) {
+    event.preventDefault();
+    if (event.target.nodeName !== 'IMG') {
+        return
+    } else {
+        const instance = basicLightbox.create(
+        `<img src="${event.target.dataset.source}">`);
+    
+        instance.show();
+    }
+};
+
+
+
+
+
+
+
+
+
+
+// {
+        //     onShow: () => {
+        //         document.addEventListener("click", onGaleryClick);
+        //     },
+            // onClose: () => {
+            //     document.removeEventListener("click", onGaleryClick);
+            // },
